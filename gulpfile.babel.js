04bd4565008dada1119727c2b416c98bf1e9a5e1
Deglier Amorim <degliersa@gmail.com>
2017-03-19 01:30:05 -0300

    removido bulma do yarn

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,18 +7,79 @@ import concat from 'gulp-concat'
 import browserSync from 'browser-sync'
 import plumber from 'gulp-plumber'
 import sourcemap from 'gulp-sourcemaps'
+import fs from 'fs'
 import sass from 'gulp-sass'
+import clean from 'gulp-clean'
 import cssmin from 'gulp-cssmin'
 import prefix from 'gulp-autoprefixer'
 
+// Definindo a origem dos pacotes a serem incluidos
+const origin = {
+  bower: 'bower_components',
+  node: 'node_modules'
+}
+
+// Definindo a saida final
+const dirOut = {
+  dir: 'dist',
+  assets: 'dist/assets'
+}
+
+// Definindo a entrada inicial
+const dirIn = {
+  dir: 'src',
+  assets: 'dist/assets'
+}
+
+
+// Tarefa que apaga (caso já exista) a pasta 'dist': diretorio onde ficam todos os arquivos finalizados/compilados
+gulp.task('clean-all', () => {
+  console.log('Deletando a o diretório ./dist')
+  return gulp.src(dirOut.dir, {read: false})
+    .pipe(clean())
+})
+
+// Limpa a pasta js do diretorio final afim de evitar cacheamento entre o desenvolvimento
+gulp.task('clean-js', () => {
+  return gulp.src(`${dirOut.assets}/js`, {read: false})
+    .pipe(clean())
+})
+
+// Limpa a pasta css do diretorio final afim de evitar cacheamento entre o desenvolvimento
+gulp.task('clean-css', () => {
+  return gulp.src(`${dirOut.assets}/css`, {read: false})
+    .pipe(clean())
+})
+
+// Limpa a pasta fonts do diretorio final afim de evitar cacheamento entre o desenvolvimento
+gulp.task('clean-fonts', () => {
+  return gulp.src(`${dirOut.assets}/fonts`, {read: false})
+    .pipe(clean())
+})
+
+
+// Copia todos os aquivos html para a dist
 gulp.task('copyHtml', () =>{
   return gulp.src('src/**/*.html')
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest(dirOut.dir))
     .pipe(browserSync.reload({stream: true}))
 })
 
+// copia as fontes indicadas com seus repesctivos diretorios advindos da npm/bower
+gulp.task('copyFonts',['clean-fonts'], () => {
+
+  let dirs = [
+    `${origin}/font-awesome/fonts/*`
+  ]
+
+  return gulp.src(dirs)
+    .pipe(gulp.dest(`${dirOut.assets}/fonts/`))
+    .pipe(browserSync.reload({stream: true}))
+})
+
+
 gulp.task('tsc', () => {
-  return gulp.src('src/assets/js/**/*.ts')
+  return gulp.src(`${dirOut.assets}/js/**/*.ts`)
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(tsc({
@@ -26,19 +87,24 @@ gulp.task('tsc', () => {
       module: 'amd'
     }))
     .pipe(sourcemap.write('./'))
-    .pipe(gulp.dest('dist/assets/js/'))
+    .pipe(gulp.dest(`${dirOut.assets}/js`))
     .pipe(browserSync.reload({stream: true}))
 })
 
 
 gulp.task('sass', () => {
-  return gulp.src('src/assets/sass/main.sass')
+  let entry = [
+    `${origin.bower}/bulma/bulma.sass`,
+    `${origin.bower}/font-awesome/scss/font-awesome.scss`,
+    `${dirIn.dir}/sass/main.sass`
+  ]
+  return gulp.src(entry)
     .pipe(sourcemap.init())
     .pipe(sass({style: 'compress',includePaths: ['scss'],onError: browserSync.notify}))
     .pipe(prefix(['last 15 versions','> 1%','ie 8','ie 7'],{cascade: true}))
     .pipe(cssmin())
     .pipe(sourcemap.write('./'))
-    .pipe(gulp.dest('dist/assets/css/'))
+    .pipe(gulp.dest(`${dirOut.assets}/css`))
     .pipe(browserSync.reload({stream:true}));
 
 });
@@ -58,15 +124,15 @@ gulp.task('sass', () => {
 gulp.task('server', () => {
   browserSync({
     server: {
-      baseDir: './dist'
+      baseDir: `${dirOut.dir}`
     }
   })
 })
 
 gulp.task('watch', () => {
   gulp.watch('src/**/*.html', ['copyHtml'])
-  gulp.watch('src/assets/js/**/*.ts', ['tsc'])
-  gulp.watch('src/assets/sass/**/*.{sass, scss, css}', ['sass'])
+  gulp.watch(`${dirIn.assets}/js/**/*.ts`, ['tsc'])
+  gulp.watch(`${dirIn.assets}/sass/**/*.{sass, scss, css}`, ['sass'])
 })
 
 gulp.task('default', ['copyHtml', 'tsc', 'sass', 'server', 'watch'])

b9e405b99c63286e06653aa67186d3adc89c6b4e
Deglier Amorim <degliersa@gmail.com>
2017-03-24 02:51:43 -0300

    task para tratar arquivos ts

---
+++ tasks/typescript.js
@@ -0,0 +1,14 @@
+import {src, dest} from 'gulp'
+import config from '../config'
+
+import tsc from 'gulp-typescript'
+import plumber from 'gulp-plumber'
+
+let tsConfig = tsc.createProject('tsconfig.json')
+
+export const tsCompiler = () => src(config.ts.readPaths)
+    .pipe(tsc({
+      target: 'es5',
+      module: 'amd'
+    }))
+    .pipe(dest(`${config.in}${config.ts.out}`))

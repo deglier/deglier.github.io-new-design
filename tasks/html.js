61d6c83a645b5e3ec6be24b4c5c022b1bcfe742c
Deglier Amorim <degliersa@gmail.com>
2017-03-24 02:56:25 -0300

    refatoração da tarefa que trata os arquivos html

--- tasks/html.js
+++ tasks/html.js
@@ -1,7 +1,16 @@
 import {src, dest} from 'gulp'
-import paths from '../config'
+import config from '../config'
 import browserSync from 'browser-sync'
+import pug from 'gulp-pug'
 
-export const copyHtml = () => src(`${paths.in}/**/*.html`)
-  .pipe(dest(paths.out))
+export const copyHtml = () => src(`${config.in}/**/*.html`)
+  .pipe(dest(config.out))
+  .pipe(browserSync.reload({stream: true}))
+
+
+export const compileViews = () => src(config.pug.readPaths)
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(dest(config.out))
   .pipe(browserSync.reload({stream: true}))

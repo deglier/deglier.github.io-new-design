1c645149cf74e3d2eb332483de821e9ce8e82e5a
Deglier Amorim <degliersa@gmail.com>
2017-03-29 14:11:27 -0300

    implements sprite svg

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,9 +6,11 @@ import {copyRequirejs, devJs} from './tasks/javascript'
 import {server} from './tasks/server'
 import {compileViews} from './tasks/html'
 import {copyFonts} from './tasks/fonts'
-import {compressImages} from './tasks/images'
+import {spriteSvg, compressImages} from './tasks/images'
 import {devWatch} from './tasks/watch'
 import {tsCompiler} from './tasks/typescript'
+
+export const test = series(spriteSvg)
 
 export const dev = series(clean, parallel(copyRequirejs, series(tsCompiler, devJs), compileViews, devSass, copyFonts, series(compressImages)), parallel(server, devWatch))
 
